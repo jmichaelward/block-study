1c38341ca397a0cf496433674079bf740bb3b3af
Jeremy Ward <jeremy.ward@webdevstudios.com>
2018-08-26 20:44:32 -0500

    More plugin scaffolding.

--- src/Plugin.php
+++ src/Plugin.php
@@ -1,9 +1,23 @@
 <?php
-
-
 namespace JMichaelWard\LearningGutenberg;
 
-
+/**
+ * Class Plugin
+ *
+ * @package JMichaelWard\LearningGutenberg
+ */
 class Plugin {
+	/**
+	 * Run the plugin.
+	 */
+	public function run() {
+		$this->register_hooks();
+	}
 
+	/**
+	 * Register any custom hooks required by this plugin.
+	 */
+	public function register_hooks() {
+		// @TODO Gotta add some hooks, aye?
+	}
 }
